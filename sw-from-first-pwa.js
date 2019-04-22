57e39c5062c652b8b66e7c20f52af7ee40827ebf
Andrey Lamzin <lamzin.an@gmail.com>
2019-04-22 10:06:54 +0300

    Добавлен пример другого, работающего sw

---
+++ sw-from-first-pwa.js
@@ -0,0 +1,45 @@
+var cacheName = 'lkam01';
+var filesToCache = [
+	//'/temp/pportal',
+	'/temp/pportal/index.html',
+	'/temp/pportal/app.js'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+
+
+//clean cache (удаляем ненужное!)
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+
+
+//получаем данные из кеша, если нет сети...
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
