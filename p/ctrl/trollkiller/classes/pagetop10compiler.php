4bcfcdeafb8669c4cff550a193076cc948fef2b4
Andrey Lamzin <lamzin.an@gmail.com>
2019-08-19 16:28:57 +0300

    Шаблоны для генерации страницы списка самых активных троллебойцев

---
+++ p/ctrl/trollkiller/classes/pagetop10compiler.php
@@ -0,0 +1,8 @@
+<?php
+//require_once __DIR__ . '/pageusercompiler.php';
+
+class PageTop10Compiler {
+	public function __construct() {
+		//TODO
+	}
+}
---
+++ p/ctrl/trollkiller/task_tktop10.php
@@ -0,0 +1,41 @@
+<?php
+//It run 15 every hour:05:00
+
+require_once __DIR__ . '/../../../q/q/config.php';
+require_once __DIR__ . '/../../../q/q/mysql.php';
+require_once __DIR__ . '/../../../q/q/utils.php';
+require_once __DIR__ . '/classes/pagetop10compiler.php';
+//require_once __DIR__ . '/classes/pageusercompiler.php';
+
+if (!function_exists('getallheaders')) {
+	function getallheaders(){
+		return [];
+	}
+}
+/**
+ * @class Task_TrollKillerTop10 - генерация страницы /portfolio/web/userscripts/trollkiller/list/ и всех 10 страниц
+ * 	/portfolio/web/userscripts/trollkiller/user/N пользователей, входящих в Топ 10
+*/
+class Task_TrollKillerTop10 {
+	public $uid = 0;
+	
+	public function __construct()
+	{
+		$this->table = 'trollkiller';
+		$sql = 'SELECT t.uid, COUNT(t.id) AS cnt, tui.name, tui.imgpath, u.name, u.surname
+			FROM trollkiller AS t 
+			
+			LEFT JOIN trollkiller_userinfo AS tui ON tui.uid = t.uid
+			
+			LEFT JOIN ausers AS u ON u.id = t.uid
+			
+			GROUP BY uid ORDER BY cnt DESC LIMIT 10';
+		
+		$aData = query($sql);
+		$oCompiler = new PageTop10Compiler();
+		$oCompiler->compile($aData);
+	}
+	
+	
+}
+new Task_TrollKillerTop10();
