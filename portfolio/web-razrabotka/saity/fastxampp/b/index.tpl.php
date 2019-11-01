9ceaf334915edb99d61f00a2303ecaf4a816f4fc
Andrey Lamzin <lamzin.an@gmail.com>
2019-11-01 13:08:34 +0300

    Exclude img/random/index.php from cache

--- portfolio/web-razrabotka/saity/fastxampp/b/index.tpl.php
+++ portfolio/web-razrabotka/saity/fastxampp/b/index.tpl.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Отправить письмо в будущее с мобильного</title>
 	<meta http-equiv="content-type" content="text/html;charset=windows-1251" />
+	<title>Отправить письмо в будущее с мобильного</title>
+	<script src="/portfolio/web-razrabotka/saity/fastxampp/js/landcacherswinstaller.js"></script>
 	<meta name="keywords" content="письмо в будущее,смс в будущее, письмо самому себе,всем в 2050 году" />
 	<link rel="stylesheet" href="./css/main.css" type="text/css">
 	<meta name="viewport" content="initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, width=device-width, height=device-height,target-densitydpi=device-dpi, user-scalable=no" />
@@ -56,10 +57,10 @@
 				<label>Введите символы с изображения</label>
 			</div>
 			<div>
-				<img id="cap" width="200" src="img/random/index.php?r=<?=rand(10, 99994)?>">
+				<img id="cap" width="200" src="img/random/index.php">
 			</div>
 			<div>
-				<a href="#" onclick="document.getElementById('cap').setAttribute('src', 'img/random/index.php?r=' + Math.random()); return false;">Обновить рисунок</a>
+				<a href="#" onclick="document.getElementById('cap').setAttribute('src', 'img/random/index.php); return false;">Обновить рисунок</a>
 			</div>
 			<div>
 				<input type="text" name="code" autocomplete="off"> <input type="submit" value="Отправить">
@@ -67,6 +68,23 @@
 			
 		</form>
 	</div>
+	<script>window.rid = '00';</script><?php /* //TODO! */?>
+	<script src="/p/sbadmin2/vendor/jquery/jquery.min.js"></script>
+    <script src="/j/sources/landlib/net/rest.js"></script>
+    <script src="/portfolio/web-razrabotka/saity/fastxampp/js/land_cache_client.js"></script>
+    <script src="/portfolio/web-razrabotka/saity/fastxampp/b/js/cacheclient.js"></script>TODO
+    <script>
+        /*new FxCacheClient(); TODO
+        var id = parseInt(window.rid, 10), o = {};
+        if (isNaN(id)) {
+            console.log('id is NaN');
+        }
+        o.id = id;
+        o.url = location.href.split('?')[0];
+        o.type = o.url.indexOf('/portfolio/') == -1 ? 2 : 1;
+        Rest._token = 'open';
+        Rest._post(o, function(){}, '/p/stat/c.jn/', function(){});*/
+    </script>
 </body>
 
 </html>
