54ee07c880a247017afba61ecf13ed175bc74e74
andrey <lamzin.an@gmail.com>
2019-12-23 12:46:19 +0300

    Exclude from cache lang change urls

--- portfolio/web-razrabotka/saity/fastxampp/js/fxcacheclient.js
+++ portfolio/web-razrabotka/saity/fastxampp/js/fxcacheclient.js
@@ -43,6 +43,7 @@ class FxCacheClient extends LandCacheClient {
 			'https://andryuxa.ru/portfolio/web-razrabotka/saity/fastxampp/extensions_for_php_7.3.12_linux/?c=ru',
 			'https://andryuxa.ru/portfolio/web-razrabotka/saity/fastxampp/download.php?c=ru',
 			'https://andryuxa.ru/portfolio/web-razrabotka/saity/fastxampp/download.php?c=en',
+			'https://andryuxa.ru/portfolio/web-razrabotka/saity/fastxampp/download.php',
 			'/portfolio/web-razrabotka/saity/fastxampp/f/save.php'
 		];
 		return o;
