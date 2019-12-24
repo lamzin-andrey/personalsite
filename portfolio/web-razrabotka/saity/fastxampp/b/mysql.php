4ccf2f538efbd62bf9a4b2d7b3d944ebc7e5e8ab
Andrey Lamzin <lamzin.an@gmail.com>
2019-12-24 14:32:06 +0300

    Fix future mail encoding

--- portfolio/web-razrabotka/saity/fastxampp/b/mysql.php
+++ portfolio/web-razrabotka/saity/fastxampp/b/mysql.php
@@ -96,8 +96,8 @@ function dbvalue($cmd) {
 function setConnection() {
 	$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD) or die('Error connect to mysql');
 	mysqli_select_db($link, DB_NAME) or die('Error select db ' . DB_NAME);
-	//mysqli_query($link, 'SET NAMES UTF8');
-	mysqli_query($link, 'SET NAMES CP1251');
+	mysqli_query($link, 'SET NAMES UTF8');
+	//mysqli_query($link, 'SET NAMES CP1251');
 	return $link;
 }
 function db_escape(&$s) {
