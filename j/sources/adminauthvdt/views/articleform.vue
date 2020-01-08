22454758798f65136000801a50172a2e65cd41f5
Andrey Lamzin <lamzin.an@gmail.com>
2020-01-08 14:52:34 +0300

    Начало фронта для vue-good-table

---
+++ j/sources/adminauthvdt/views/articleform.vue
@@ -0,0 +1,409 @@
+<template>
+    <form class="user" method="POST" action="/p/savearticle.jn/" @submit="onSubmit" novalidate id="aricleform">
+		<selectb4 v-model="category" @input="setDataChanges" :label="$t('app.Sections')" id="category" :data="pagesCategories" validators="'required'"></selectb4>
+		<inputb4 v-model="title" @input="setDataChanges" type="text" :placeholder="$t('app.Title')" :label="$t('app.Title')" id="title" validators="'required'"></inputb4>
+        <inputb4 v-model="url"  @input="setDataChanges"  type="url" :label="$t('app.Url')" :placeholder="$t('app.Url')" id="url" ></inputb4>
+        <inputb4 v-model="heading" @input="setDataChanges" id="heading" type="text" :label="$t('app.Heading')" :placeholder="$t('app.Heading')"  validators="'required'"></inputb4>
+        <textareab4 v-model="body" ref="articlebody" @input="setDataChanges" :counter="counter" :label="$t('app.Content')"  id="content_block" rows="12" validators="'required'"></textareab4>
+        <div class="mb-3">
+			<inputfileb4 
+						v-model="poster"
+						url="/p/articleinlineimageupload.jn/"
+						tokenImagePath="/i/token.png"
+						:listeners="posterUploadListeners"
+						:csrfToken="$root._getToken()"
+						:label="$t('app.insertImage')" id="poster" ></inputfileb4>
+		</div>
+		<img :src="defaultLogo" >
+		<inputfileb4 
+            v-model="filepath"
+            url="/p/articlelogoupload.jn/"
+            immediateleyUploadOff="true"
+            tokenImagePath="/i/token.png"
+            :progressListener="progressbarListener"
+            :listeners="fileUploadListeners"
+			:uploadButtonLabel="$t('app.Upload')"
+            :csrfToken="$root._getToken()"
+            :sendInputs="['alpha']"
+            
+         :label="$t('app.SelectLogo')" id="logotype" ></inputfileb4>
+		<div class="progress">
+			<div class="progress-bar" role="progressbar" 
+				:style="'width: ' + progressValue + '%;'" 
+				:aria-valuenow="progressValue" aria-valuemin="0" aria-valuemax="100">{{ progressValue }}%</div>
+        </div>
+		<checkboxb4  id="alpha" :label="$t('app.isMakeTransparentBg')" value="true"></checkboxb4>
+
+		<checkboxb4 id="hiddenInList"  v-model="hidden_in_list" :label="$t('app.articleHiddeninList')" value="true"></checkboxb4>
+		<checkboxb4 id="isHidden" v-model="is_hidden" :label="$t('app.is_hidden')" value="true"></checkboxb4>
+         
+
+        <div class="accordion" id="seoAccord">
+			<div class="card">
+				<div class="card-header" id="headingSeo">
+				<h5 class="mb-0">
+					<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseSeo" aria-expanded="true" aria-controls="collapseSeo">
+					SEO
+					</button>
+				</h5>
+			</div>
+
+			<div id="collapseSeo" class="collapse" aria-labelledby="headingSeo" data-parent="#seoAccord">
+				<div class="card-body">
+					<inputb4 @input="setDataChanges" v-model="menu_heading" type="text" :placeholderlabel="$t('app.menu_heading')"  ></inputb4>
+					<inputb4 @input="setDataChanges" v-model="right_menu_secondary_text" type="text" :placeholderlabel="$t('app.right_menu_secondary_text')"  ></inputb4>
+					<inputb4 @input="setDataChanges" v-model="description" type="text" placeholderlabel="meta[name=description]" maxlength="200" id="description"></inputb4>
+					<inputb4 @input="setDataChanges" v-model="keywords" type="text" placeholderlabel="meta[name=keywords]" ></inputb4>
+					<inputb4 @input="setDataChanges" v-model="og_title" type="text" placeholderlabel="og:title"  ></inputb4>
+					<inputb4 @input="setDataChanges" v-model="og_description" type="text" placeholderlabel="og:description"  ></inputb4>
+					
+					<img :src="defaultSocImage" style="max-width:100px; max-height:100px;">
+					<inputfileb4 
+						v-model="og_image"
+						url="/p/articleogimageupload.jn/"
+						tokenImagePath="/i/token.png"
+						:listeners="ogImageUploadListeners"
+						:csrfToken="$root._getToken()"
+						:label="$t('app.SelectOgImage')" id="og_image" ></inputfileb4>
+				</div>
+				</div>
+			</div>
+			
+		</div>
+		<div class="float-right ">
+			<div id="Saver" class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-delay="3000">
+				<div class="toast-header">
+					<strong class="mr-auto">Info</strong>
+					<small class="text-muted"></small>
+					<button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="toast-body">
+					{{ $t('app.SaveCompleted') }}
+				</div>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+
+        <p class="text-right my-3">
+            <button  class="btn btn-primary">{{ $t('app.Save') }}</button>
+        </p>
+        
+    </form>
+
+</template>
+<script>
+
+    //Компонент для отображения инпута ввода текста bootstrap 4
+    Vue.component('inputb4', require('../../landlib/vue/2/bootstrap/4/inputb4.vue').default);
+    Vue.component('selectb4', require('../../landlib/vue/2/bootstrap/4/selectb4.vue').default);
+    Vue.component('checkboxb4', require('../../landlib/vue/2/bootstrap/4/checkboxb4.vue').default);
+    Vue.component('textareab4', require('../../landlib/vue/2/bootstrap/4/textareab4.vue').default);
+    Vue.component('inputfileb4', require('../../landlib/vue/2/bootstrap/4/inputfileb4/inputfileb4.vue').default);
+
+    export default {
+        name: 'articleform',
+        //вызывается раньше чем mounted
+        data: function(){
+			let _data  = {
+				//Значение title
+				title:'',
+				//Значение body
+				body:'',
+				//Значение url
+				url:'',
+				//Значение heading
+				heading:'',
+				//Путь к загруженному логотипу
+				filepath:'',
+				//Параметры для кастомного прогресс-бара инпута загрузки лого
+				progressbarListener:{
+					onProgress: {
+						f: this.onProgress,
+						context:this
+					}
+				},
+				fileUploadListeners : {
+					onSuccess:{
+						f : this.onSuccessUploadLogo,
+						context:this
+					}
+				},
+				//Логотип статьи
+				defaultLogo: '/i/64.jpg',
+				//Изображение для соц. сетей
+				defaultSocImage: '/i/64.jpg',
+				//Путь к изображению по умолчанию
+				defaultLogoValue: '/i/64.jpg',
+				//Исходное имя файл изображения
+				srcFileName: '',
+				//Значение по умолчанию для кастомной шкалы прогресса
+				progressValue : 0,
+				//Выбранная категория
+				category : 1,
+				//
+				pagesCategories : [
+					{id:1, name:"One"},
+					{id:2, name:"Two"}
+				],
+				//Идентификатор редактируемой статьи
+				id : 0,
+				//Чтобы передать в textareab4 true пришлось определить
+				counter: true,
+
+				//Скрывать вообще
+				is_hidden:false,
+				//Скрывать в списке статей
+				hidden_in_list:false,
+
+				//Содержимое META тега
+				description : '',
+				//Содержимое META тега
+				keywords : '',
+				//Содержимое META тега
+				og_title : '',
+				//Содержимое META тега
+				og_description : '',
+				//Содержимое META тега
+				og_image : '',
+				//Заголовок для пункта меню
+				menu_heading : '',
+				//Дополнительный текст в пункте меню
+				right_menu_secondary_text : '',
+				//Параметры для кастомного слушателя загрузки og_image
+				ogImageUploadListeners:{
+					onSuccess:{
+						f : this.onSuccessUploadOgImage,
+						context : this
+					}
+				},
+				//Переменная для хранения ссылки на инлайновое изображение
+				poster : '',
+				//Параметры для кастомного слушателя загрузки изображений, ссылки на которые вставляются в textarea
+				posterUploadListeners: {
+					onSuccess:{
+						f : this.onSuccessUploadposter,
+						context : this
+					}
+				}
+			};
+			try {
+				let jdata = JSON.parse($('#jdata').val());
+				_data.pagesCategories = jdata.pagesCategories;
+			} catch(e) {
+				console.log('opace', e);
+			}
+			return _data;
+		},
+		watch: {
+			//Чтобы известить об том, что контент отредактирован при загрузке изображения на сервер
+			og_image:function() {
+				this.$root.setDataChanges(true);
+			},
+			filepath:function() {
+				this.$root.setDataChanges(true);
+			}
+		},
+        //
+        methods:{
+			//TODO remove me
+            _alert(s) {
+                alert(s);
+			},
+			/**
+			 * @description Установитрь данные статьи для редактирования
+			 * @param {Object} data @see mysql table fields pages
+			*/
+			setArticleData(data) {
+				
+				this.category = 1001;
+				this.title = 'a';
+				this.url = 'b';
+				this.heading = 'c';
+				this.body = 'd';
+				this.filepath = this.defaultLogo = this.defaultLogoValue;
+				this.description = 'e';
+				this.keywords = 'f';
+				this.og_title = 'g';
+				this.og_description = 'h';
+				this.menu_heading = '';
+				this.right_menu_secondary_text = '';
+				this.og_image = this.defaultSocImage = this.defaultLogoValue;
+				this.hidden_in_list = false;
+				this.is_hidden = false;
+				
+				//Fix bug when edit the article more then one time...
+				Vue.nextTick(() => {
+					let url;
+					this.category = data.category_id;
+					this.title = data.title;
+					url = this.url = data.url;
+					this.heading = data.heading;
+					Vue.nextTick(() => {
+						this.setUrlWasChanged(url, this.title);
+					});
+					this.body = data.content_block;
+					this.filepath = this.defaultLogo = data.logo;
+					this.description = data.description;
+					this.keywords = data.keywords;
+					this.og_title = data.og_title;
+					this.og_description = data.og_description;
+					this.og_image = this.defaultSocImage = data.og_image;
+					this.menu_heading = data.menu_heading;
+					this.right_menu_secondary_text = data.menu_heading;
+
+					this.hidden_in_list = parseInt(data.hidden_in_list) ? true : false;
+					this.is_hidden = parseInt(data.is_hidden) ? true : false;
+				});
+			},
+			/**
+			 * @description Очистить инпуты изображений
+			*/
+			resetImages() {
+				this.defaultSocImage = this.filepath =  this.defaultLogo = this.defaultLogoValue;
+				this.og_image = '';
+			},
+			/**
+			 * @description Вставить изображение на место курсора
+			*/
+			onClickInsertImage(){
+				console.log('OI call');
+			},
+            /** 
+             * @description Кастомный прогресс для загрузкти лого
+             * @param {Number} n
+            */
+            onProgress(a) {
+                if (a <= 100 && a > 0) {
+                    this.progressValue = a;
+                }
+			},
+			/**
+			 * @description уведомляем приложение, что данные изменились
+			 */
+			setDataChanges() {
+				console.log('articlegor,setDataChanges...');
+				this.$root.setDataChanges(true);
+			},
+            /** 
+             * @description Пробуем отправить форму
+            */
+            onSubmit(evt) {
+                evt.preventDefault();
+                if (this.allRequiredFilled()) {
+					let formInputValidator = this.$root.formInputValidator;
+					this.id = this.$root.getArticleId();
+					this.url = $('#url').val();
+                    this.$root._post(
+                        this.$data,
+                        (data) => { this.onSuccessAddArticle(data, formInputValidator);},
+                        '/p/articlesave.jn/',
+                        (a, b, c) => { this.onFailAddArticle(a, b, c);}
+                    );
+                }
+			},
+			/**
+			 * @description Успешное добавление статьи
+			*/
+			onSuccessAddArticle(data, formInputValidator){
+				if (!this.onFailAddArticle(data)) {
+					return;
+				}
+				let id = parseInt(data.id);
+				if (data.status == 'ok' && id) {
+					this.$root.setArticleId(id);
+					$('#Saver').toast('show');
+					this.$root.setDataChanges(false);
+					this.$root.dataTable.search('').draw();
+				}
+			},
+			/**
+			 * @description Неуспешное добавление статьи
+			 * @return Boolean false если существует data.status == 'error'
+			*/
+			onFailAddArticle(data, b, c){
+				return this.$root.defaultFailSendFormListener(data,b, c);
+				
+			},
+			/**
+             * @description Проверяет, заполнены ли все необходимые поля
+            */
+			allRequiredFilled(){
+				return (
+					parseInt(this.category) > 0
+					&& String(this.title).length > 0
+					&& String(this.heading).length > 0
+					&& String(this.body).length > 0
+				);
+			},
+            /**
+			 * @description
+             * @param {Object} data
+            */
+            onSuccessUploadLogo(data) {
+				if (data.path) {
+					this.defaultLogo = data.path;
+				}
+				if (data.srcname) {
+					this.srcFileName = data.srcname;
+				}
+			},
+			/**
+			 * @description Обработка успешной загрузки фото ДЛЯ соц. сетей
+            */
+			onSuccessUploadOgImage(data) {
+				if (data.path) {;
+					this.og_image = this.defaultSocImage = `${window.location.protocol}//${window.location.host}${data.path}`;
+				}
+			},
+			/**
+			 * @description Обработка успешной загрузки изображения для вставки в текстовое поле
+            */
+			onSuccessUploadposter(data) {
+				if (data.path) {
+					let x = 'articlebody', n = this.$refs[x].getCursorPosition(), s, head, tail;
+					if (n > -1) {
+						s = this.body;
+						head = s.substring(0, n);
+						tail = s.substring(n);
+						s = `[html]<img src="${data.path}">[/html]`;
+						this.body = head + s + tail;
+						setTimeout(() => {
+							this.$refs[x].setCursorPosition(n + s.length);
+						}, 200);
+					}
+				}
+			},
+			/**
+			 * @description Установить факт того, что url уже редактирован в ручную и не надо его переустанавливать, транслитируя heading
+			 * @param {String} url - сохраненный ранее в базе
+			 * @param {String} heading
+			*/
+			setUrlWasChanged(url, heading){
+				let key = 'urlIsModify',
+					s = window.LandLibDom.liveTransliteGetPrefix(key) + window.TextFormat.transliteUrl(heading) + window.LandLibDom.liveTransliteGetSuffix(key);
+				if (url != s) {
+					window.LandLibDom.liveTransliteSetWasHandEdit(key, true);
+					this.url = url;
+				} else {
+					window.LandLibDom.liveTransliteSetWasHandEdit(key, false);
+				}
+			}
+        }, //end methods
+        //вызывается после data, поля из data видны "напрямую" как this.fieldName
+        mounted() {
+			window.LandLibDom.liveTranslite('#title', '#url', 'urlIsModify', '/blog/', '/');
+            var self = this;
+            /*this.$root.$on('showMenuEvent', function(evt) {
+                self.menuBlockVisible   = 'block';
+                self.isMainMenuVisible  = true;
+                self.isScrollWndVisible = false;
+                self.isColorWndVisible  = false;
+                self.isHelpWndVisible   = false;
+                self.nStep = self.$root.nStep;
+            })/**/
+            //console.log('I mounted!');
+        }
+    }
+</script>
---
+++ j/sources/adminauthvdt/views/articlesections.vue
@@ -0,0 +1,356 @@
+<template>
+<div>
+<ul class="nav nav-tabs" role="tablist">
+		<li class="nav-item">
+			<a class="nav-link" 
+				id="acatlist-tab"
+				href="#acatlist"
+				role="tab"
+				aria-controls="home"
+				aria-selected="true">{{ $t('app.List') }}</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link active"
+				id="editacat-tab"
+				data-toggle="tab"
+				href="#editacat"
+				role="tab"
+				aria-controls="profile"
+				aria-selected="false"> {{ formTabTitle }} </a>
+		</li>
+	</ul>
+	<div class="tab-content">
+		<div class="tab-pane fade " id="acatlist" role="tabpanel" aria-labelledby="list-tab">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">Список категорий статей</h5>
+					
+					<table id="articlecats" class="display table table-bordered" style="width:100%">
+						<thead>
+							<tr>
+								<th>{{ $t('app.Heading') }}</th> 
+								<th>{{ $t('app.Operations') }}</th>
+							</tr>
+						</thead>
+						<tfoot>
+							<tr>
+								<th>{{ $t('app.Heading') }}</th>
+								<th>{{ $t('app.Operations') }}</th>
+							</tr>
+						</tfoot>
+					</table>
+					
+				</div>
+			</div>
+		</div>
+		
+		<div class="tab-pane fade show active" id="editacat" role="tabpanel" aria-labelledby="edit-tab">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title"> {{ newEdit }} </h5>
+					<articlecategoryform ref="categoryform"></articlecategoryform>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</template>
+
+<script>
+	//Центровка прелоадера DataTables по центру (самоделка, но надо оформить как плагин)
+	import B4DataTablesPreloader from '../../landlib/datatables/b4datatablespreloader.js';
+	//Конец Центровка прелоадера DataTables по центру (самоделка)
+
+    
+	Vue.component('articlecategoryform', require('./articlecategoriesform.vue'));
+
+    export default {
+        name: 'articlesections',
+        //вызывается раньше чем mounted
+        data: function(){
+			let _data  = {
+				/** @property {Number}  Переменная для хранения id категории статьи запрошенной для редактирования */
+				requestedCategoryId : 0,
+				
+				/** @property {String} newEdit Переменная для Заголовка формы Добавления/ редактирования категории */
+				 newEdit : 'app.New',
+				 
+	 			/** @property {String} formTabTitle Переменная для надписи на табе формы Добавления/ редактирования категории */
+				formTabTitle : 'app.Append',
+
+				/** @property {Boolean} isChange Принимает true когда данные категории изменены, но не сохранены */
+				isChange : false,
+				 
+				//Центрируем прелоадер DataTables и добавляем в него спиннер
+				/** @property  {B4DataTablesPreloader} dataTablesPreloader */
+				dataTablesPreloader: new B4DataTablesPreloader(),
+
+				/** @property {Number} categeoryId Идентификатор редактируемой категории статьи */
+	 			categeoryId : 0,
+			};
+			return _data;
+		},
+        //
+        methods:{
+			/**
+			 * @description инициализация DataTables с данными статей
+			*/
+			initDataTables() {
+				if (this.isArticlesDataTableInitalized) {
+					return;
+				}
+				let id = '#articlecats';
+				this.isArticlesDataTableInitalized = true;
+				this.dataTable =  $(id).DataTable( {
+					'processing': true,
+					'serverSide': true,
+					'ajax': "/p/acategories/acatslist.jn/",
+					"columns": [
+						{ 
+							"data": "category_name",
+							'render' : function(data, type, row) {
+								return  data;
+							}
+						},
+						{
+							"data": "id",
+							'render' : function(data, type, row) {
+								return  `
+									<div class="form-group d-md-inline d-block ">
+										<button data-id="${data}" type="button" class="btn btn-primary j-edit-btn">
+											<i data-id="${data}" class="fas fa-edit fa-sm"></i>
+										</button>
+									</div>
+									<div class="form-group d-md-inline d-block ">
+										<button data-id="${data}" type="button" class="btn btn-danger j-rm-btn">
+											<i data-id="${data}" class="fas fa-trash fa-sm"></i>
+										</button>
+									</div>
+									<div class="form-group d-md-inline d-block ">
+										<div id="spin${data}" class="spinner-grow text-success d-none" role="status">
+											<span class="sr-only">Loading...</span>
+										</div>
+									</div>
+									`;
+							}
+						},
+						
+					],
+					language: {
+						url: '/p/datatablelang.jn/'
+					}
+				} ).on('draw', () => {
+					//Когда всё отрисовано устанавливаем обработчики событий кликов на кнопках
+					$(id + ' .j-edit-btn').click((evt) => {
+						this.onClickEditCategory(evt);
+					});
+					$(id + ' .j-rm-btn').click((evt) => {
+						this.onClickRemoveCategory(evt);
+					});
+				}).on('processing', () => {
+					//Preloader
+					if (!this.preloaderIsInitalize) {
+						//Делаем прелоадер по центру
+						this.dataTablesPreloader.setIdentifiers(id, id + '_processing', this.dataTable);
+						//this.dataTablesPreloader.configure(true, false);
+						this.dataTablesPreloader.watch();
+						this.preloaderIsInitalize = true;
+					}
+
+					//Search settings
+					if (!this.addLeftLimitOnSearchField) {
+						this.addLeftLimitOnSearchField = true;
+						let inp = $(id + '_filter input').first();
+						inp.unbind();
+						inp.on('input', () => {
+							let val = inp.val();
+							if (val.length > 4 || val.length == 0) {
+								this.dataTable.search(val).draw();
+							}
+						});
+					}
+				});
+				
+			},
+			/**
+			 * @description Click on button "Edit category"
+			 * @param {Event} evt
+			*/
+			onClickEditCategory(evt) {
+				if (this.requestedCategoryId > 0) {
+					this.alert(this.$t('app.Other_category_requested_for_edit'));
+					return;
+				}
+				this.requestedCategoryId = $(evt.target).attr('data-id');
+				$('#spin' + this.requestedCategoryId).toggleClass('d-none');
+				this.$root._get((d) => {this.onSuccessGetCategory(d);}, `/p/acategories/category.jn/?id=${this.requestedCategoryId}`, (a, b, c) => {this.onFailGetCategory(a, b, c);} );
+			},
+			/**
+			 * @description Success request category data for edit
+			 * @param {Object} data
+			*/
+			onSuccessGetCategory(data) {
+				if (!this.onFailGetCategory(data)) {
+					return;
+				}
+				this.setCategoryId(data.id);
+				this.$refs.categoryform.setCategoryData(data);
+				setTimeout(() => {
+					this.setDataChanges(false);
+				}, 1000);
+				$('#editacat-tab').tab('show');
+			},
+			/**
+			 * @description Failed request category data for edit
+			 * @return Boolean
+			*/
+			onFailGetCategory(data, b ,c) {
+				$('#spin' + this.requestedCategoryId).toggleClass('d-none');
+				this.requestedCategoryId = 0;
+				return this.$root.defaultFailSendFormListener(data, b ,c);
+			},
+			/**
+			 * @description Click on button "Remove category"
+			 * @param {Event} evt
+			*/
+			onClickRemoveCategory(evt) {
+				this.$root.confirmDialogArticleArgs  = {i:$(evt.target).attr('data-id')};
+				this.$root.b4ConfirmDlgParams.title = this.$t('app.Are_You_Sure_drop_Article_Category') + '?';
+				this.$root.b4ConfirmDlgParams.body = this.$t('app.Click_Ok_button_for_remove');
+				this.$root.b4ConfirmDlgParams.onOk = {
+					f : this.onClickConfirmRemoveCategory,
+					context:this
+				};
+				this.$root.setConfirmDlgVisible(true);
+			},
+			/**
+			 * @description Click on button "OK" on confirm dialog Remove article
+			 * @param {Event} evt
+			*/
+			onClickConfirmRemoveCategory() {
+				let args = this.$root.confirmDialogArticleArgs;
+				this.$root._post(args, (data) => {this.onSuccessRemove(data);}, '/p/acategories/removecategory.jn/', (data) => {this.onFailRemove(data);})
+				this.$root.setConfirmDlgVisible(false);
+			},
+			/**
+			 * @description Добавляем поведение для таба SEO - он должен показываться только когда активна не первая вкладка
+			 * @param data - Данные с сервера
+			*/
+			onSuccessRemove(data) {
+				if (data.status == 'ok') {
+					if (data.id) {
+						let tr = $(`#articlecats button[data-id=${data.id}]`).first().parents('tr').first();
+						tr.remove();
+					}
+				} else {
+					this.onFailRemove(data);
+				}
+			},
+			/**
+			 * @description Добавляем поведение для таба SEO - он должен показываться только когда активна не первая вкладка
+			 * @param data - Данные с сервера
+			 */
+			onFailRemove(data) {
+				if (data.status == 'error' && data.msg) {
+					this.$root.alert(data.msg);
+					return;
+				}
+				this.$root.alert($t('DefaultFail'));
+			},
+			/**
+			 * @description Установить id редактируемой категории
+			 * @param {Number} id 
+			*/
+			setCategoryId(id) {
+				this.categoryId = id;
+				let key = 'app.New',
+					key2 = 'app.Append';
+				
+				if (id > 0) {
+					key2 = key =  'app.Edit';
+				}
+				this.newEdit = this.$root.$t(key);
+				this.formTabTitle = this.$root.$t(key2);
+			},
+			/**
+			 * @description Получить id редактируемой категории
+			 * @return Number
+			*/
+			getCategoryId() {
+				return this.categoryId;
+			},
+			/**
+			 * @see isChange
+			 * @param {Boolean} isChange 
+			 */
+			setDataChanges(isChange) {
+				this.isChange = isChange;
+			},
+			/**
+			 * @description Добавляем 
+			 */
+			initSeotab() {
+				$('#acatlist-tab').on('click', (ev) => {
+					ev.preventDefault();
+					if (this.isChange) {
+						//Сменим тексты диалога, чтобы было ясно, что речь идёт именно о переключении на новую вкладку
+						this.$root.b4ConfirmDlgParams.title = this.$t('app.Are_You_Sure_Stop_Edit_Article') + '?';
+						//И сменим обработчик, чтобы удалялась именно статья
+						this.$root.b4ConfirmDlgParams.onOk = {
+							f : this.onClickConfirmLeaveEditTab,
+							context:this
+						};
+						//Покажем диалог
+						this.$root.setConfirmDlgVisible(true);
+					} else {
+						this.gotoCategoriesListTab();
+					}
+				});
+				$('#editacat-tab').on('shown.bs.tab', (ev) => {
+					this.setDataChanges(false);
+				});
+			},
+			/**
+			 * @description Обработка OK на диалоге подтверждения переключения между вкладками
+			*/
+			onClickConfirmLeaveEditTab() {
+				this.gotoCategoriesListTab();
+				//Скроем диалог
+				this.$root.setConfirmDlgVisible(false);
+			},
+			/**
+			 * @description Показать список категорий, сбросить id редактируемой категории, установить флаг "данные не изменялись" и очистить форму
+			*/
+			gotoCategoriesListTab() {
+				$('#acatlist-tab').tab('show');
+				$('#ariclecategoriesform')[0].reset();
+				this.setCategoryId(0);
+				this.setDataChanges(false);
+			},
+			/**
+			 * @description Тут локализация некоторых параметров, которые не удается локализовать при инициализации
+			 */
+			localizeParams() {
+				//Заголовок формы редактиорвания
+				this.newEdit = this.$root.$t('app.New');
+				this.formTabTitle = this.$root.$t('app.Append');
+			},
+        }, //end methods
+        //вызывается после data, поля из data видны "напрямую" как this.fieldName
+        mounted() {
+			this.localizeParams();
+			this.initDataTables();
+			this.initSeotab();
+            
+            /*this.$root.$on('showMenuEvent', function(evt) {
+                self.menuBlockVisible   = 'block';
+                self.isMainMenuVisible  = true;
+                self.isScrollWndVisible = false;
+                self.isColorWndVisible  = false;
+                self.isHelpWndVisible   = false;
+                self.nStep = self.$root.nStep;
+            })/**/
+            //console.log('I mounted!');
+        }
+    }
+</script>
