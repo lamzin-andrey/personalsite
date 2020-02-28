ff5200f96c238157a7bbfed6e8448351ad47b2bc
andrey <lamzin.an@gmail.com>
2020-02-28 14:23:59 +0300

    Set field 255

--- j/webpack.mix.js
+++ j/webpack.mix.js
@@ -31,6 +31,6 @@ mix.styles([
 	], './../s/app.css');
 
 //Учёт времени - админка
-mix.js('sources/cronfrnd/app.js', 'h.js'); //crn
+//mix.js('sources/cronfrnd/app.js', 'h.js'); //crn
 //Страница логина второй уровень (там только конфиг для cache service worker)
-mix.js('sources/sploginpagecacheclient/app.js', 'i.js');
+//mix.js('sources/sploginpagecacheclient/app.js', 'i.js');
