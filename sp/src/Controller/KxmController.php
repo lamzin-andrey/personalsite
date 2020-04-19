99c6c9bd595e38e8e9f9eea7401a2eb466601052
andrey <lamzin.an@gmail.com>
2020-04-19 18:02:14 +0300

    Продолжаем пилить форму добавления вопросов

--- sp/src/Controller/KxmController.php
+++ sp/src/Controller/KxmController.php
@@ -2,19 +2,38 @@
 
 namespace App\Controller;
 
+use App\Service\AppService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\KxmQuestFormType;
 
 class KxmController extends AppBaseController
 {
     /**
+     * @property KxmQuest $_oKxmQuest вопрос викторины
+    */
+    private $_oKxmQuest = null;
+    /**
      * @Route("/kxm", name="kxm")
      */
-    public function index()
+    public function index(AppService $oAppService)
     {
-        die('Hello kxm');
+        $oForm = $this->_createForm();
         return $this->render('kxm/index.html.twig', [
             'controller_name' => 'KxmController',
+            'formtoken' => $oAppService->getFormTokenValue($oForm)
         ]);
+    }
+
+    /**
+     *
+     * @param $
+     * @return
+    */
+    private function _createForm() : FormInterface
+    {
+        $this->_oForm = $this->createForm(KxmQuestFormType::class, $this->_oKxmQuest);
+        return $this->_oForm;
     }
 }
---
+++ sp/src/Entity/KxmQuest.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\KxmQuestRepository")
+ */
+class KxmQuest
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=512)
+     */
+    private $body;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $var1;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $var2;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $var3;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $var4;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $var_right;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): self
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getVar1(): ?string
+    {
+        return $this->var1;
+    }
+
+    public function setVar1(string $var1): self
+    {
+        $this->var1 = $var1;
+
+        return $this;
+    }
+
+    public function getVar2(): ?string
+    {
+        return $this->var2;
+    }
+
+    public function setVar2(string $var2): self
+    {
+        $this->var2 = $var2;
+
+        return $this;
+    }
+
+    public function getVar3(): ?string
+    {
+        return $this->var3;
+    }
+
+    public function setVar3(string $var3): self
+    {
+        $this->var3 = $var3;
+
+        return $this;
+    }
+
+    public function getVar4(): ?string
+    {
+        return $this->var4;
+    }
+
+    public function setVar4(string $var4): self
+    {
+        $this->var4 = $var4;
+
+        return $this;
+    }
+
+    public function getVarRight(): ?int
+    {
+        return $this->var_right;
+    }
+
+    public function setVarRight(int $var_right): self
+    {
+        $this->var_right = $var_right;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
