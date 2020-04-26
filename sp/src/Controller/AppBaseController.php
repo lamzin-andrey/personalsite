77716fd0c924567d108e34ed85d09e26a79790b7
andrey <lamzin.an@gmail.com>
2020-04-26 13:04:19 +0300

    Fix заголовков ответа для android browser

--- sp/src/Controller/AppBaseController.php
+++ sp/src/Controller/AppBaseController.php
@@ -57,6 +57,35 @@ class AppBaseController extends AbstractController
 		if ($sThemeVersion == 'a2') {
 			$view = 'themes/a2/' . $view;
 		}
+
+
+		if (!$response) {
+			//Fix for android browser 2 - Симфони почему то всегда возвращает ему Content-type: text/xml
+
+			//Например http://mh.loc/sp/public/login GET (если захочется воспроизвеяти баг)
+			//Пример заголовков android browser 2.3.6 (если захочется воспроизвеяти баг):
+
+			//Host: andryuxa.ru
+			//User-Agent: Mozilla/5.0 (Linux; U; Android 2.3.6; ru-ru; GT-S5363 Build/GINGERBREAD) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1
+			//Accept: application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5
+			//Accept-Language: ru-RU, en-US
+			//Accept-Encoding: gzip
+			//Accept-Charset: utf-8, iso-8859-1, utf-16, *;q=0.7
+			//Pragma: no-cache
+			//Cache-Control: no-cache
+			//Upgrade-Insecure-Requests: 1
+			//Connection: keep-alive
+			//Cookie: PHPSESSID=c5fb48c5d803777cc47c8a1a9af5c56b; REMEMBERME=QXBwXEVudGl0eVxBdXNlcnM6YkdrPToxNjE5MzM4Mzk3OmYzNTU4MjVlYTI5ZGIzYWQ1ZTgxNjdhZThmZGUzYTlmYzk0MmJkOTZiODM3Y2M3ZmI1MWIzYThiYTM3NGE5NTY%3D; s=2990ef8af82effdfef2d298f84680ce537dea86a; sv=a2
+
+
+			$sUa = $oRequest->server->get('HTTP_USER_AGENT');
+			if ($sUa != 'Mozilla/5.0 (Linux; U; Android 2.3.6; ru-ru; GT-S5363 Build/GINGERBREAD) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1') {
+				return parent::render($view, $parameters, $response);
+			}
+			$response = new Response();
+			header('Content-Type: text/html');
+			$response->headers->set('Content-Type', 'text/html; charset=UTF-8');
+		}
 		return parent::render($view, $parameters, $response);
 	}
 }
