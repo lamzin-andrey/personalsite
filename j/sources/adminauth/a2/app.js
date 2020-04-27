16544216931b2b865d7a991f2de7d2cee85a57a0
andrey <lamzin.an@gmail.com>
2020-04-27 12:07:39 +0300

    Сайдбар для темы android browser 2.3

--- j/sources/adminauth/a2/app.js
+++ j/sources/adminauth/a2/app.js
@@ -3,13 +3,31 @@
  * такими как форма поиска, скрытие и показ сайдбара с разделами, окошком поиска.
 */
 function AppLayout() {
-	
+	this.accordionSidebar = e('accordionSidebar');
+	this.mainLayout = e('content-wrapper')
 }
 /**
  * @description Обработка клика на кнопке меню
+ * Надо показать сайдбар.
 */
 AppLayout.prototype.onClickMenuBtn = function(event) {
-	alert('Hello!');
+	var that = window.appLayout;
+	that.hideScreens();
+	show(that.accordionSidebar);
+}
+/**
+ * @description Обработка клика на кнопке закрыть меню
+ * Надо скрыть сайдбар и показать основной контент
+*/
+AppLayout.prototype.onClickSidebarCloseButton = function(event) {
+	var that = window.appLayout;
+	that.hideScreens();
+	show(that.mainLayout);
+}
+
+AppLayout.prototype.hideScreens = function() {
+	hide(this.accordionSidebar);
+	hide(this.mainLayout);
 }
 
 
