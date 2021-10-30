621da045f2f660dda87da2855aaf48c83ad526b0
andrey <lamzin.an@gmail.com>
2021-10-30 17:01:12 +0300

    Add user-agent view page; Fix translation bug

--- ua/index.php
+++ ua/index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="WINDOWS-1251">
 		<link rel="canonical" href="https://andryuxa.ru/">
-		<meta name="viewport" content="	initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no,target-densitydpi=device-dpi,	width=device-width,height=device-height,shrink-to-fit=no">
+		<meta name="viewport" content="	initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no,target-densitydpi=device-dpi,width=device-width,height=device-height,shrink-to-fit=no">
 		<meta name="description" content="Как узнать свой User-Agent? Здесь вы можете видеть user-agent вашего устройства. Ничего лишнего, только userAgent.">
 		<title>Ваш user agent</title>
 		<meta name="yandex-verification" content="6b0be8bdc8966848" />
