5c35abe68f4e209ef77c4f8e57ac98ada1f5d9b9
andrey <lamzin.an@gmail.com>
2021-11-07 16:23:52 +0300

    FD version. location from ssl page only to ssl pages

---
+++ d/drive/fd/j/options.js
@@ -0,0 +1,38 @@
+function initOptions() {
+	e('boLeave').onclick = onClickOptionLeave;
+	e('boChangeLang').onclick = onClickOptionChangeLanguage;
+	e('boChangeTheme').onclick = onClickOptionChangeTheme;
+	e('boCloseOpts').onclick = onClickOptionExit;
+}
+
+function showOptions() {
+	addClass('hBotMenu', 'hide');
+	removeClass('hBotOptions', 'hide');
+}
+function hideOptions() {
+	addClass('hBotMenu', 'hide');
+	addClass('hBotOptions', 'hide');
+}
+
+function onClickOptionLeave() {
+	var ref = storage('referrer');
+	if (!ref || ref == location.href) {
+		ref = 'https://google.com';
+	}
+	hideOptions();
+	location.href = ref;
+}
+
+function onClickOptionChangeLanguage() {
+	hideOptions();
+	goURL(roota2 + '/clang');
+}
+
+function onClickOptionChangeTheme() {
+	goURL(root); // Это не догма, мути что хочешь
+	hideOptions();
+}
+
+function onClickOptionExit() {
+	hideOptions();
+}
