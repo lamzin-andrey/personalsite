ad34277067de3012900efd4ce83d75f84147e15f
andrey <lamzin.an@rosfines.ru>
2023-11-18 19:45:51 +0300

    WUSP php7.33

--- d/drive/a2/j/catalog/sharelinksetter.js
+++ d/drive/a2/j/catalog/sharelinksetter.js
@@ -1,7 +1,7 @@
 window.ShareLinkSetter = {
 	setLink:function(s){
 		var ssl = 'https://',
-			sc = ~location.href.indexOf(ssl) ? sssl : 'http://';
+			sc = ~location.href.indexOf(ssl) ? ssl : 'http://';
 		e('clink').innerHTML = sc + s;
 		attr('clink', 'href', sc + s);
 	}
