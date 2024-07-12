a4633f263af48df50b715021a6090e7355c0a2fb
andrey <lamzin.an@rosfines.ru>
2024-07-12 15:57:07 +0300

    Drive - подготовка к выгрузке

--- sp/src/Controller/UsbController.php
+++ sp/src/Controller/UsbController.php
@@ -41,7 +41,7 @@ use StdClass;
 class UsbController extends AbstractController
 {
 
-    private const VERSION = '11';
+    private const VERSION = '12';
 
     /** @property string $backendRoot subdirectory with root symfony project */
     private  $backendRoot = '/sp/public';
