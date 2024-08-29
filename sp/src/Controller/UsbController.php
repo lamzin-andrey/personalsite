a201529b1fd8d7ac986ea3b404ab2aab685f9927
andrey <lamzin.an@rosfines.ru>
2024-08-29 17:03:03 +0300

    WUSB добавить CacheClient чтобы письма не дублировались

--- sp/src/Controller/UsbController.php
+++ sp/src/Controller/UsbController.php
@@ -41,7 +41,7 @@ use StdClass;
 class UsbController extends AbstractController
 {
 
-    private const VERSION = '23';
+    private const VERSION = '24';
 
     /** @property string $backendRoot subdirectory with root symfony project */
     private  $backendRoot = '/sp/public';
