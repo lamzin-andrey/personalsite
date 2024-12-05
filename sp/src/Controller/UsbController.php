843b75a972da58f3e7285daae6e7cfaf26cf9c5b
andrey <lamzin.an@rosfines.ru>
2024-12-05 18:01:19 +0300

    WUSB PC Добавить пунк о проекте в fd, w

--- sp/src/Controller/UsbController.php
+++ sp/src/Controller/UsbController.php
@@ -43,7 +43,7 @@ use StdClass;
 class UsbController extends AbstractController
 {
 
-    private const VERSION = '37';
+    private const VERSION = '38';
 
     /** @property string $backendRoot subdirectory with root symfony project */
     private  $backendRoot = '/sp/public';
