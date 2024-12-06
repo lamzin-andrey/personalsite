27c23f4077c042cff2984c047f53b9bf114668da
andrey <lamzin.an@rosfines.ru>
2024-12-06 17:56:58 +0300

    WUSB PC try fix sw

--- sp/src/Controller/UsbController.php
+++ sp/src/Controller/UsbController.php
@@ -43,7 +43,7 @@ use StdClass;
 class UsbController extends AbstractController
 {
 
-    private const VERSION = '38';
+    private const VERSION = '39';
 
     /** @property string $backendRoot subdirectory with root symfony project */
     private  $backendRoot = '/sp/public';
