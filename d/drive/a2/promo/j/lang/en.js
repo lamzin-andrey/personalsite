d9e393aa6d9549766102565eddd2634769e3d100
andrey <lamzin.an@rosfines.ru>
2024-12-13 15:03:57 +0300

    WUSB PC На страницу About ссылку на ПС в текст.

--- d/drive/a2/promo/j/lang/en.js
+++ d/drive/a2/promo/j/lang/en.js
@@ -5,5 +5,7 @@ window.langEn = {
 	'Вы не имеете право на доступ к этой странице' : 'You have not access to this page',
 	'Нажмите, чтобы скачать' : '<div>The WebUSB project means a few more gigabytes in the cloud for your files.</div>\
 				<div>Your smartphone may be outdated, but files from WebUSB will still be accessible from its default browser - that\'s the idea of this project.</div>',
+	'bUA' : 'User Agreement',
+	'bPA' : 'Privace Policy',
 	'' : ''
 };
