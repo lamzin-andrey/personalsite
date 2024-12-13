091895e17769a8d75a16ddc8d0c9ebaee86791a7
andrey <lamzin.an@rosfines.ru>
2024-12-13 16:55:21 +0300

    WUSB PC Обновление версий - если нет версии в х, не редиректить, просто рефрешить.

--- d/drive/a2/j/vers/getvers.js
+++ d/drive/a2/j/vers/getvers.js
@@ -3,12 +3,17 @@ function gVonLoad() {
 	VersionReq.get(window, gVonVersion);
 }
 function gVonVersion(v){
-	if (storage('vers') != v) {
+	var sv = storage('vers'), H = HttpQueryString;
+	if (sv != v) {
 		storage('vers', v);
-		goURL(roota2 + '/newversion/?v=' + v);
+		if (sv) {
+			goURL(roota2 + '/newversion/?v=' + v);
+		} else {
+			goURL(H.setVariable(location.href, 'v', v));
+		}
 		return;
 	}
 	if ($_GET['v'] != v) {
-		goURL(HttpQueryString.setVariable(location.href, 'v', v));
+		goURL(H.setVariable(location.href, 'v', v));
 	}
 }
